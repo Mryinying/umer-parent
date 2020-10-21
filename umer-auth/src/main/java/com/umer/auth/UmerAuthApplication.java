27e1181316561a6bde11ff522d39856e49599ca9
wang_zy <18895613681@163.com>
2020-10-21 18:07:42 +0800

    6 commit auth

---
+++ umer-auth/src/main/java/com/umer/auth/UmerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.umer.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmerAuthApplication.class, args);
+	}
+
+}
---
+++ umer-auth/src/test/java/com/umer/auth/UmerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umer.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
